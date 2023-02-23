f06cd015f118b0b4357d762755aa2b79bd694e27
jkrueger721 <johnathankrueger@me.com>
2023-02-22 23:17:16 -0500

    added a few more methods to vector

--- arrays/vector.c
+++ arrays/vector.c
@@ -23,8 +23,8 @@ struct myVector{
   int (*ptrVectorIsEmpty)(vector *);
   int (*ptrVectorCapacity)(vector *);
   int (*ptrVectorAtIndex)(vector *);
-  void (*ptrVectorPush)(vector *);
-  void (*ptrVectorPrepend)(vector *);
+  int (*ptrVectorPush)(vector *);
+  int (*ptrVectorPrepend)(vector *);
   void (*ptrVectorInsert)(vector *);
   int (*ptrVectorPop)(vector *);
   int (*ptrVectorDelete)(vector *);
@@ -39,8 +39,13 @@ struct myVector{
 #define VECTOR_INIT(vec) vector vec;\
  vector_init(&vec)
 
-
-
+int size(vector* v);
+int capacity(vector* v);
+int resize(vector* v, int newCapacity);
+int push(vector* v, void* item);
+int at(vector* v , void* item);
+int is_empty(vector* v);
+int prepend(vector* v , void* item);
 
 int main(void)
 {
@@ -53,11 +58,11 @@ int main(void)
 void vector_init(vector *v){
   v->ptrVectorSize = size;
   v->ptrVectorCapacity = capacity;
-  // v->ptrVectorIsEmpty = is_empty;
-  // v->ptrVectorAtIndex = at;
-  // v->ptrVectorPush = push;
-  // v->ptrVectorInsert = insert;
-  // v->ptrVectorPrepend = prepend;
+  v->ptrVectorIsEmpty = is_empty;
+  v->ptrVectorAtIndex = at;
+  v->ptrVectorPush = push;
+  v->ptrVectorInsert = insert;
+  v->ptrVectorPrepend = prepend;
   // v->ptrVectorPop = pop;
   // v->ptrVectorDelete = delete;
   // v->ptrVectorRemoveItem = vec_remove;
@@ -78,17 +83,24 @@ int capacity(vector* v){
   return v->vectorList.capacity;
 }
 
-int resize(vector* v, int capacity)
+int resize(vector* v, int newCapacity)
 {
 
   int status = UNDEFINE;
   if(v)
   {
-      void **items = realloc(v->vectorList.items, sizeof(void*) * capacity);
+    if(v->vectorList.size == v->vectorList.capacity){
+      newCapacity = v->vectorList.capacity * 2;
+    }else if (v->vectorList.size == v->vectorList.capacity / 4)
+    {
+      newCapacity = v->vectorList.capacity / 2;
+    }
+    
+      void **items = realloc(v->vectorList.items, sizeof(void*) * newCapacity);
       if (items)
       {
         v->vectorList.items = items;
-        v->vectorList.capacity = capacity;
+        v->vectorList.capacity = newCapacity;
         status = SUCCESS;
       }
       
@@ -97,9 +109,83 @@ int resize(vector* v, int capacity)
 
 }
 
-// int is_empty(vector* v){
-//   return 0;
-// }
-  
+int push(vector* v, void* item){
 
+  int status = UNDEFINE;
+
+  if(v){
+    if(v->vectorList.capacity == v->vectorList.size){
+      status = resize(v, v->vectorList.capacity * 2);
+      if(status != UNDEFINE){
+        v->vectorList.items[v->vectorList.size++] = item;
+      }
+    }else {
+
+        v->vectorList.items[v->vectorList.size++] = item;
+        status = SUCCESS;
+    }
+  }
+
+  return status;
+}
+
+int at(vector* v , void* item){
+  int index = UNDEFINE;
+  if (v)
+  {
+     for (int i = 0; i < v->vectorList.size; i++)
+     {
+       if (item == v->vectorList.items[i])
+       {
+         index = i;
+         return index;
+       }
+       
+     }
+     
+  }
+
+  return index;
   
+}
+int is_empty(vector* v){
+
+  int result = 0;
+  for (int i = 0; i < v->vectorList.size; i++)
+  {
+     if(v->vectorList.items[i] == NULL){
+      result = 1;
+    }
+  }
+  return result;
+}
+
+int prepend(vector* v , void* item){
+  int status = UNDEFINE;
+  if(v && item){
+    
+    status = insert(v, item, 0);
+    
+  }
+  return status;
+}
+
+int insert(vector* v , void* item, int pos){
+  int status = UNDEFINE;
+  if(v){
+    if (v->vectorList.size == v->vectorList.capacity)
+    {
+      status = resize(v , v->vectorList.capacity);
+    }
+    for (int i = v->vectorList.size - 1; i >= pos; i--)
+    {
+      v->vectorList.items[i + 1] = v->vectorList.items[i];
+
+    }
+
+    v->vectorList.items[pos] = item;
+    
+    status = SUCCESS;
+  }
+  return status;
+}
