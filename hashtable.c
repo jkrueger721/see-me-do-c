b4cd3b24e2e2edf8f91864ea424327c016591f27
jkrueger721 <johnathankrueger@me.com>
2023-03-16 22:56:54 -0400

    current progress

--- hashtable.c
+++ hashtable.c
@@ -1,27 +1,64 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 
 const int max = 10;
 int size = 0;
 
 int valueArray[max]={NULL};
+int hashArray[max]={NULL};  
 
-int hash(int k, int m){
+int hash(int k){
 
-   int h;
-   do
-   {
-      h = k % m;
-      h++;
-   } while (valueArray[h] != NULL && h <= max);
-   return h;
+   return k % max;
 
+}
+
+int rehash(int key)
+{
+ int i;
+
+ i = (key+1) % max;
+
+ return i;
 }
 void add(int k, int v){
   int index;
-  index = hash(k,max);
+  index = hash(k);
   valueArray[index] = v;
+}
+
+bool exits(int key){
+     key = hash(key);
+
+  for (int i = 0; i < max; i++)
+  {
+   int compare = valueArray[i];
+    if (compare == key)
+    {
+      return true;
+    }
+        
+  }
+  return false;
+  
+}
+int get(int key){
+  int value;
+  int index = hash(key);
+  if (valueArray[index] != NULL)
+  {
+    value = valueArray[index];
+    return value;
+  }
+  
+  return -1;
+}
+
+void remove(int key){
+  int h = hash(key);
+  valueArray[h] = NULL;
 }
 
 int main(void)
@@ -29,6 +66,5 @@ int main(void)
   add(34,54);
   add(23,45);
   add(31,456);
-  printf("%d \n", hash(5435234, max));
 }
 
