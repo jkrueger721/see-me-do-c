8fe295bb121f1c697a631074f996540a863cf81f
jkrueger721 <johnathankrueger@me.com>
2023-03-28 22:25:29 -0400

    added search method

--- BSTree.c
+++ BSTree.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 
 typedef struct BTSnode {
   int data;
@@ -14,6 +16,13 @@ typedef struct BTSnode {
   
   return newNode;
   
+}
+
+bool search(BTSnode* root, int data){
+  if(root == NULL) return false;
+  else if(root->data == data) return true;
+  else if(data <= root->data) return search(root->left, data);
+  else return search(root->right, data);
 }
 
 BTSnode* insert(BTSnode* root, int data){
