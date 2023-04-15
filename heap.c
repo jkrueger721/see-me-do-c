4c0b8128d4c6c3bf73a048a0f54e962d070b3c20
jkrueger721 <johnathankrueger@me.com>
2023-04-14 22:38:21 -0400

    added insert method

--- heap.c
+++ heap.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 
+#define MAX_SIZE 15
 int size = 0;
 /*
 parent (x -1) /2
@@ -13,10 +14,19 @@ void swap(int* a , int* b ){
   *a = temp;
 }
 
+int parent(int n){
+  return (n - 1) / 2;
+}
+int leftChild(int n) {
+    return 2 * n + 1;
+}
+int rightChild(int n) {
+    return 2 * n + 2;
+}
 void heapify(int arr[], int n, int i){
   int max = i; 
-  int l = 2 * i + 1;
-  int r = 2 * i + 2;
+  int l = leftChild(i);
+  int r = rightChild(i);
 
   if (l < n && arr[l] > arr[max]) {
      max = l;
@@ -39,6 +49,22 @@ void buildHeap(int arr[], int n ){
   }
 }
 
+void insert(int arr[],int n, int *index){
+    
+   if (*index >= MAX_SIZE) {
+    printf("Heap if fulle");
+    return;
+   }  
+   arr[*index] = n;
+    *index++;
+
+    int i = *index - 1;
+  while(i != 0 && arr[parent(i)] < arr[i]){
+    swap(&arr[parent(i)], &arr[i]);
+    i = parent(i);
+  }
+
+}
 
 int main(void){
   
