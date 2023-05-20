6b623da6e66cbbe22e95f6a9ee5029c6681c9624
jkrueger721 <johnathankrueger@me.com>
2023-05-19 22:39:59 -0400

    find path function

--- graph.c
+++ graph.c
@@ -179,6 +179,15 @@ void breadth_first_search(graph* g, int start){
   
 }
 
+void find_path(int start, int end, int parents[]){
+  if ((start == end) || (end == -1)) {
+    printf("\n%d", start);
+  }else {
+    find_path(start, parents[end], parents);
+    printf(" %d", end);
+  }
+}
+
 int main(){
   graph g;
   init_graph(&g, false);
